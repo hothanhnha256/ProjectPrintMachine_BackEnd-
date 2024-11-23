6d26a9f9ca0e94c412194b71140939839b758583
Khoa1204HCMUT <156278110+mp777888@users.noreply.github.com>
2024-11-23 19:33:35 +0700

    update file module 23-11-2024

--- src/main/java/com/learingspring/demo_spring/File/controller/FileController.java
+++ src/main/java/com/learingspring/demo_spring/File/controller/FileController.java
@@ -6,13 +6,11 @@ import com.learingspring.demo_spring.File.dto.response.FileResponse;
 import com.learingspring.demo_spring.File.service.FileService;
 import com.learingspring.demo_spring.exception.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.List;
 
 @RestController
 @RequestMapping("/files")
