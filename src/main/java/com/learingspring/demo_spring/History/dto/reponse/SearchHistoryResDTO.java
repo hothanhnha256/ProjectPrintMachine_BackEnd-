3722bcf71c5e22abe22d2333185493d955135ad0
sondang2004 <125138558+sondang2004@users.noreply.github.com>
2024-12-08 21:48:01 +0700

    Update SearchHistoryResDTO.java

--- src/main/java/com/learingspring/demo_spring/History/dto/reponse/SearchHistoryResDTO.java
+++ src/main/java/com/learingspring/demo_spring/History/dto/reponse/SearchHistoryResDTO.java
@@ -1,12 +1,18 @@
 package com.learingspring.demo_spring.History.dto.reponse;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
+import com.learingspring.demo_spring.File.dto.response.FileResponse;
+import com.learingspring.demo_spring.PrintMachine.dto.response.AvailablePrintersResponse;
+import com.learingspring.demo_spring.User.dto.response.UserResponse;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @SuppressWarnings("common-java:DuplicatedBlocks")
